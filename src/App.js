eed1bd9bbf917b5364619f61889d12f9db695e50
Piyush Kuhad <piyushkuhad@gmail.com>
2020-08-20 09:14:28 +0530

    Setup redirect on login

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Switch, Route} from 'react-router-dom';
+import {Switch, Route, Redirect} from 'react-router-dom';
 import { connect } from 'react-redux';
 
 import Homepage from './pages/homepage/Homepage';
@@ -78,15 +78,24 @@ class App extends React.Component {
         <Switch>
           <Route exact path="/" component={Homepage} />
           <Route path="/shop" component={ShopPage} />
-          <Route path="/auth" component={Authentication} />
+          <Route 
+            exact path="/auth"  
+            render={
+              () => this.props.currentUser ? (<Redirect to="/" />) : (<Authentication />)
+            }
+          />
         </Switch>
       </div> 
     )
   }
 }
 
+const mapStateToProps = ({ user }) => ({
+  currentUser: user.currentUser
+})
+
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 })
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
