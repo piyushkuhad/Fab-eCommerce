b060dabee8809dca25358c62d93d067ffecb7298
Piyush Kuhad <piyushkuhad@gmail.com>
2020-08-24 16:29:08 +0530

    moved directory and shop data into its own reducer, created respective selectors and updated corresponding components with new redux flow for directory/shop

--- src/pages/shoppage/ShopPage.js
+++ src/pages/shoppage/ShopPage.js
@@ -1,31 +1,18 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 
-import SHOP_DATA from './shopData';
-import CollectionPreview from '../../components/collection-preview/CollectionPreview';
-
+import CollectionsOverview from '../../components/collections-overview/CollectionsOverview';
+import CollectionPage from '../collection-page/CollectionPage';
 import './ShopPage.scss';
 
-class ShopPage extends React.Component {
-    
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            collections: SHOP_DATA
-        }
-    }
-    
-    render(){
-        const {collections} = this.state;
-        //console.log(collections);
-        return(
-            <div className="shop-page">
-                {collections.map(({id, ...otherCollectionProps}) => (
-                    <CollectionPreview key={id} {...otherCollectionProps}/>
-                ))}
-            </div>
-        )
-    }
+const ShopPage = ({ match }) => {
+    //console.log(match.path);
+    return(
+        <div className="shop-page">
+            <Route exact path={`${match.path}`} component={CollectionsOverview} />
+            <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
+        </div>
+    )
 }
 
 export default ShopPage;
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,11 @@
 import { createStore, applyMiddleware } from 'redux';
+import { persistStore } from 'redux-persist';
 import logger from 'redux-logger';
 
 import rootReducer from './rootReducer';
 
 const middleware = [logger]
 
-const store = createStore(rootReducer, applyMiddleware(...middleware));
+export const store = createStore(rootReducer, applyMiddleware(...middleware));
 
-export default store;
+export const persistor = persistStore(store);
