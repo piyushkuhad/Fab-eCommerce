db3f2fbdc63196423e5d71f0371c36191b477a19
Piyush Kuhad <piyushkuhad@gmail.com>
2020-08-24 19:58:27 +0530

    Removed redux-logger

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,13 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore } from 'redux';
 import { persistStore } from 'redux-persist';
-import logger from 'redux-logger';
+//import logger from 'redux-logger';
 
 import rootReducer from './rootReducer';
 
-const middleware = [logger]
+//const middleware = [logger]
 
-export const store = createStore(rootReducer, applyMiddleware(...middleware));
+//export const store = createStore(rootReducer, applyMiddleware(...middleware));
+
+export const store = createStore(rootReducer);
 
 export const persistor = persistStore(store);
